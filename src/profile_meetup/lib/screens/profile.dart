b8f2d7ac853eec5ec253e991f9ffd281605ba1cb
Flutter Guadalajara <crystaldonut@gmail.com>
2020-03-13 00:07:20 -0600

    Ejemplo de Layout

--- src/profile_meetup/lib/screens/profile.dart
+++ src/profile_meetup/lib/screens/profile.dart
@@ -1,102 +1,145 @@
 import 'package:flutter/material.dart';
+import 'package:gdl_watermark/gdl_watermark.dart';
 
 class Profile extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter GDL.'),
-        backgroundColor: Colors.black,
+        title: Text('Flutter Meetup Guadalajara'),
+        backgroundColor: Colors.orange,
       ),
-      body: Column(
-        children: <Widget>[
-          Text(
-            'Edición Marzo',
-            style: TextStyle(fontSize: 18),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.message, color: Colors.black, size: 30,),
-                  onPressed: () => print('Mensaje'),
-                ),
-                CircleAvatar(
-                  backgroundColor: Colors.black,
-                  child: Text('FG'),
-                  radius: 30,
-                ),
-                IconButton(
-                    icon: Icon(Icons.phone, size: 30),
-                    onPressed: () => print('Llamar')
-                )
-              ],
+      body: Stack(children: <Widget>[
+
+        Column(
+          children: <Widget>[
+            Spacer(
+              flex: 1,
             ),
-          ),
-          Divider(),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 20),
-            child: Row(
-              children: <Widget>[
-               Expanded(child: _fila1('10', 'Repos')),
-               Expanded(child: _fila1('30', 'Seguidores')),
-               Expanded(child: _fila1('7', 'Comentarios')),
-               Expanded(child: _fila1('45', 'Siguiendo')),
-              ],
+            Expanded(
+              flex: 5,
+              child: getTitle(),
             ),
-          ),
-          Divider(),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 30, horizontal: 50),
-            child: Text(
-              'Flutter GDL, una comunidad con ganas de aprender y mejorar. Estamos para compartir, ayudar y aprender.',
-              textAlign: TextAlign.center,
-              style: TextStyle(),
+            Expanded(
+              flex: 12,
+              child: getContactHeadline(),
             ),
-          ),
-          Divider(),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 20),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: Column(
-                    children: <Widget>[
-                      _fila1('Twitter', '@fluttergdl'),
-                      SizedBox(height: MediaQuery.of(context).size.height * 0.02,),
-                      _fila1('Phone', '+52 (462) 264 1353'),
-                    ],
+            Expanded(flex: 1, child: Divider()),
+            Spacer(
+              flex: 2,
+            ),
+            Expanded(
+              flex: 8,
+              child: getUserStats(),
+            ),
+            Expanded(flex: 1, child: Divider()),
+            Spacer(),
+            Flexible(
+              flex: 15,
+              child: getTextFragment(),
+            ),
+            Spacer(),
+            Expanded(flex: 1, child: Divider()),
+            Spacer(
+              flex: 2,
+            ),
+            Flexible(
+              flex: 20,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 5,
+                    child: Column(
+                      children: <Widget>[
+                        Flexible(child: _fila1('Twitter', '@fluttergdl')),
+                        Flexible(child: _fila1('Phone', '+52 (462) 264 1353')),
+                      ],
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: Column(
-                    children: <Widget>[
-                      _fila1('Ubicación', 'Guadalajara, Jal.'),
-                      SizedBox(height: MediaQuery.of(context).size.height * 0.02,),
-                      _fila1('Email', 'se.israel.mc@gmail.com'),
-                    ],
+                  Expanded(
+                    flex: 5,
+                    child: Column(
+                      children: <Widget>[
+                        Flexible(child: _fila1('Ubicación', 'Guadalajara, Jal.')),
+                        Flexible(child: _fila1('Email', 'se.israel.mc@gmail.com')),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+            Spacer(
+              flex: 25,
+            ),
+          ],
+        ),
+        Positioned(
+          width: MediaQuery.of(context).size.width,
+          bottom: 0,
+          child: Align(
+              child: FlutterGdl.showBottomWatermark()),
+        ),
+      ]),
+    );
+  }
+
+  Widget getTextFragment() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      child: Text(
+        'Flutter GDL, una comunidad con ganas de aprender y mejorar. Estamos para compartir, ayudar y aprender.',
+        textAlign: TextAlign.center,
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.black,
-        child: Icon(Icons.portrait),
-        onPressed: () => print(''),
+    );
+  }
+
+  Widget getUserStats() {
+    return Row(
+      children: <Widget>[
+        Expanded(child: _fila1('10', 'Repos')),
+        Expanded(child: _fila1('30', 'Seguidores')),
+        Expanded(child: _fila1('7', 'Comentarios')),
+        Expanded(child: _fila1('45', 'Siguiendo')),
+      ],
+    );
+  }
+
+  Widget getContactHeadline() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.message,
+            color: Colors.black,
+            size: 30,
+          ),
+          onPressed: () => print('Mensaje'),
+        ),
+        CircleAvatar(
+          backgroundColor: Colors.black,
+          child: Text('FG'),
+          radius: 30,
+        ),
+        IconButton(icon: Icon(Icons.phone, size: 30), onPressed: () => print('Llamar'))
+      ],
+    );
+  }
+
+  Widget getTitle() {
+    return Center(
+      child: Text(
+        'Edición Marzo',
+        style: TextStyle(fontSize: 18),
       ),
     );
   }
 
   Widget _fila1(String titulo, String subtitulo) => Column(
-    children: <Widget>[
-      Text(titulo, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-      Text(subtitulo),
-    ],
-  );
+        children: <Widget>[
+          Flexible(
+              child: Text(titulo, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20))),
+          Flexible(child: Text(subtitulo)),
+        ],
+      );
 }
